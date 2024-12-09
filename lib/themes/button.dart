212b212b4e260f2c07595219c4d87e2299b66671
c4hyo <mdcnugroho@gmail.com>
2024-12-09 22:59:40 +0700

    membuat tema untuk button

---
+++ lib/themes/button.dart
@@ -0,0 +1,80 @@
+import '../utility/constants/colors.dart';
+import '../utility/constants/theme.dart';
+import 'package:flutter/material.dart';
+
+class AppButtonStyle {
+  AppButtonStyle._();
+
+  static final filled = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      elevation: 1,
+      foregroundColor: AppColors.white,
+      backgroundColor: AppColors.primary,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(AppThemes.radius),
+      ),
+      padding: const EdgeInsets.symmetric(
+        vertical: 12,
+        horizontal: 24,
+      ),
+      textStyle: AppThemes.appFonts().bodySmall?.copyWith(
+            color: AppColors.white,
+            fontWeight: FontWeight.w600,
+          ),
+      minimumSize: const Size(
+        double.infinity,
+        48,
+      ),
+      iconColor: AppColors.white,
+      disabledIconColor: AppColors.grey,
+      disabledBackgroundColor: AppColors.primary.withOpacity(.25),
+      disabledForegroundColor: AppColors.white,
+    ),
+  );
+
+  static final outlined = OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      backgroundColor: AppColors.white,
+      side: BorderSide(
+        color: AppColors.primary,
+        width: 1.5,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(AppThemes.radius),
+      ),
+      padding: const EdgeInsets.symmetric(
+        vertical: 12,
+        horizontal: 24,
+      ),
+      textStyle: AppThemes.appFonts().bodySmall?.copyWith(
+            color: AppColors.primary,
+            fontWeight: FontWeight.w600,
+          ),
+      iconColor: AppColors.primary,
+      disabledIconColor: AppColors.grey,
+      minimumSize: const Size(
+        double.infinity,
+        48,
+      ),
+      disabledBackgroundColor: AppColors.white,
+      disabledForegroundColor: AppColors.white,
+    ),
+  );
+}
+
+Color getButtonColor(ButtonColor color) {
+  switch (color) {
+    case ButtonColor.PRIMARY:
+      return AppColors.primary;
+    case ButtonColor.ERROR:
+      return AppColors.error;
+    case ButtonColor.SUCCESS:
+      return AppColors.success;
+    case ButtonColor.INFO:
+      return AppColors.info;
+    case ButtonColor.WARNING:
+      return AppColors.warning;
+    default:
+      return AppColors.primary;
+  }
+}
