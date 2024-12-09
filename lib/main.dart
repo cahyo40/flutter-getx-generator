f5e4ed84b41524762752fe447fbc54af396bdc22
c4hyo <mdcnugroho@gmail.com>
2024-12-09 23:00:01 +0700

    testing widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'package:clean_arch/widgets/loading.dart';
+
+import 'themes/button.dart';
+import 'utility/constants/colors.dart';
+import 'utility/constants/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -12,10 +17,33 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          centerTitle: true,
+          iconTheme: IconThemeData(
+            color: AppColors.white,
+            size: FontSize.x2l,
+          ),
+          elevation: .25,
+          titleTextStyle: AppThemes.appFonts().titleMedium?.copyWith(
+                fontWeight: FontWeight.w600,
+                color: AppColors.text,
+              ),
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primary,
+        ),
+        textTheme: AppThemes.appFonts(),
+        inputDecorationTheme: AppThemes.appInputDecoration(),
+        elevatedButtonTheme: AppButtonStyle.filled,
+        outlinedButtonTheme: AppButtonStyle.outlined,
+      ),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: SafeArea(
+          child: AppLoading(
+            message: "Mohon tunggu...",
+          ),
         ),
       ),
     );
