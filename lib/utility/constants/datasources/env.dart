015f96e37009d03f3f6c760b750bfde73be193f8
cahyo40 <mdcnugroho@gmail.com>
2024-12-22 21:55:23 +0700

    init api

--- lib/utility/constants/datasources/env.dart
+++ lib/utility/constants/datasources/env.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: unused_field
 
+import 'package:clean_arch/app/config.dart';
+
 enum Environment { dev, prod }
 
 class Env {
@@ -8,12 +10,12 @@ class Env {
   static String setEnv(Environment env) {
     switch (env) {
       case Environment.dev:
-        _source = "";
+        _source = AppConfig.baseUrlDev;
         break;
       case Environment.prod:
-        _source = "";
+        _source = AppConfig.baseUrlProd;
         break;
     }
-    return _source = "";
+    return _source = AppConfig.baseUrlDev;
   }
 }
