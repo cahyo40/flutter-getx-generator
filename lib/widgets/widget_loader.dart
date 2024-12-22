9187ddafb769196c3b86d46e16257d9d9f84da13
cahyo40 <mdcnugroho@gmail.com>
2024-12-22 18:36:41 +0700

    buat widget buat loader

---
+++ lib/widgets/widget_loader.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../utility/constants/colors.dart';
+import '../utility/constants/theme.dart';
+import 'loading.dart';
+
+class AppLoader extends StatelessWidget {
+  final RxBool isLoading;
+  final Widget child;
+  final Widget? loadingWidget;
+  final String? errorMessage;
+  const AppLoader({
+    super.key,
+    required this.isLoading,
+    required this.child,
+    this.errorMessage,
+    this.loadingWidget,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => isLoading.isTrue
+          ? loadingWidget ?? const AppLoading()
+          : errorMessage != null
+              ? Center(
+                  child: Text(
+                    errorMessage!,
+                    style: AppThemes.appFonts().titleMedium!.copyWith(
+                          color: AppColors.error,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                )
+              : child,
+    );
+  }
+}
