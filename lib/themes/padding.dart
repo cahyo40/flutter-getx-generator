eee98cfcaf1cecbae0fea649fd9c74067e14d6b4
cahyo40 <mdcnugroho@gmail.com>
2024-12-25 09:08:21 +0700

    membuat app padding

---
+++ lib/themes/padding.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppPadding {
+  static EdgeInsets paddingList = const EdgeInsets.all(24);
+  static EdgeInsets paddingCard = const EdgeInsets.fromLTRB(16, 24, 16, 16);
+  static EdgeInsets paddingInput = const EdgeInsets.only(bottom: 16);
+  static EdgeInsets paddingButton = const EdgeInsets.only(top: 16);
+  static EdgeInsets paddingForm = const EdgeInsets.fromLTRB(16, 24, 16, 24);
+}
