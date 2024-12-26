df7c1c505358dc4b9b5eccc1fd543f8ee001a86a
cahyo40 <mdcnugroho@gmail.com>
2024-12-26 10:20:34 +0700

    update widgetloader

--- lib/widgets/widget_loader.dart
+++ lib/widgets/widget_loader.dart
@@ -1,39 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 import '../themes/colors.dart';
 import '../themes/theme.dart';
 import 'loading.dart';
 
 class AppLoader extends StatelessWidget {
-  final RxBool isLoading;
+  final bool isLoading;
   final Widget child;
   final Widget? loadingWidget;
   final String? errorMessage;
-  const AppLoader({
-    super.key,
-    required this.isLoading,
-    required this.child,
-    this.errorMessage,
-    this.loadingWidget,
-  });
+  final Widget? errorWidget;
+  const AppLoader(
+      {super.key,
+      required this.isLoading,
+      required this.child,
+      this.errorMessage,
+      this.loadingWidget,
+      this.errorWidget});
 
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () => isLoading.isTrue
-          ? loadingWidget ?? const AppLoading()
-          : errorMessage != null
-              ? Center(
-                  child: Text(
-                    errorMessage!,
-                    style: AppThemes.appFonts().titleMedium!.copyWith(
-                          color: AppColors.error,
-                          fontWeight: FontWeight.bold,
-                        ),
-                  ),
-                )
-              : child,
-    );
+    if (isLoading == true) {
+      return loadingWidget ?? const AppLoading();
+    } else {
+      if (errorMessage != null) {
+        return errorWidget ??
+            Center(
+              child: Text(
+                errorMessage!,
+                style: AppThemes.appFonts().titleMedium!.copyWith(
+                      color: AppColors.error,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+            );
+      } else {
+        return child;
+      }
+    }
   }
 }
