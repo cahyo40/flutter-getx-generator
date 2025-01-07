c3c3d40786c25cb08e9d3b376215fda70054f0f4
cahyo40 <mdcnugroho@gmail.com>
2025-01-07 18:01:44 +0700

    add function at database

--- lib/utility/constants/datasources/database.dart
+++ lib/utility/constants/datasources/database.dart
@@ -5,4 +5,20 @@ class AppDatabase {
   static final db = GetStorage();
   static const String token = 'db-token';
   static const String refreshToken = 'db-refresh-token';
+
+  static Future<void> write(String key, dynamic value) async {
+    await db.write(key, value);
+  }
+
+  static String? read(String key) {
+    return db.read(key);
+  }
+
+  static Future<void> remove(String key) async {
+    await db.remove(key);
+  }
+
+  static Future<void> erase() async {
+    await db.erase();
+  }
 }
